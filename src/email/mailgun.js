2c5cb1055f6af5a6c567cee7369ee5ebb569378d
Romulus Tsai 蔡仲明 <urakagi@gmail.com>
2019-05-21 13:36:47 +0800

    Add mailgun unset support

--- src/email/mailgun.js
+++ src/email/mailgun.js
@@ -1,7 +1,16 @@
-const mailgun = require("mailgun-js")(
-  {apiKey: process.env['MAILGUN_API_KEY'], domain: process.env['MAILGUN_DOMAIN']});
+const Config = require('../polis-config');
+
+let mailgun = null;
+if (Config.get('MAILGUN_API_KEY')) {
+  mailgun = require("mailgun-js")(
+    {apiKey: Config.get('MAILGUN_API_KEY'), domain: Config.get('MAILGUN_DOMAIN')});
+}
 
 function sendText(sender, recipient, subject, text) {
+  if (!mailgun) {
+    return;
+  }
+
   console.log("sending email with Mailgun: " + [sender, recipient, subject, text].join("\n"));
 
   return new Promise(function (resolve, reject) {
